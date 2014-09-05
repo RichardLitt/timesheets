65d6f4fee06f9acfb8b6aadf140d231326997343
Richard Littauer <richard.littauer@gmail.com>
2014-09-04 21:36:11 -0700

    Init

---
+++ index.js
@@ -0,0 +1,12 @@
+(function(){
+  
+  var moment = require('moment');
+  var handelbars = requite('handelbars');
+  
+  var today = moment().format();
+
+  console.log(today);
+
+  // Make a way of templating this thing. 
+
+})();
